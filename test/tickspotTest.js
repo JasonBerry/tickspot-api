0285ac8d79d2709ffc4fe50b0f1a1ae67699c069
Jason Berry <jason@thevagabond.net>
2013-06-28 15:38:08 +1000

    Fix tests so that they run asyncronously

--- test/tickspotTest.js
+++ test/tickspotTest.js
@@ -5,49 +5,88 @@ var Tickspot = require('../lib/tickspot'),
 
 describe('Tickspot', function () {
     var ts = new Tickspot('mycompany', 'myemail', 'mypassword');
+    nock(ts.host)
+        .persist()
+        .post('/api/testAuth', 'email=' + ts.email + '&password=' + ts.password).reply(201, '<foo />')
+        .post('/api/okay').reply(201, '<foo />')
+        .post('/api/bad').reply(400, 'Bad Request')
+        .post('/api/notXML').reply(201, '<foo')
+        .post('/api/clients').reply(201, '<?xml version="1.0" encoding="UTF-8"?>\
+            <clients type="array">\
+              <client>\
+                <id type="integer">12341</id>\
+                <name>Starfleet Command</name>\
+              </client>\
+              <client>\
+                <id type="integer">12342</id>\
+                <name>The Vulcans</name>\
+              </client>\
+              <client>\
+                <id type="integer">12343</id>\
+                <name>The Cardassians</name>\
+              </client>\
+            </clients>');
 
     describe('#makeRequest()', function () {
-        var method = 'foo';
-        var scope = nock(ts.host)
-                    .post('/api/' + method, 'email=' + ts.email + '&password=' + ts.password)
-                    .reply(201, 'OK');
-        var req = ts.makeRequest(method);
-
-        it('should add email and password to every request', function () {
-            scope.isDone().should.be.ok;
+        it('should add email and password to the request', function (done) {
+            var req = ts.makeRequest('testAuth');
+            req.then(function () {
+                Q.isFulfilled(req).should.be.ok;
+                done();
+            });
         });
-
         it('should return a promise', function () {
-            Q.isPromise(req).should.be.ok;
+            Q.isPromise(ts.makeRequest('okay')).should.be.ok;
+        });
+        it('should resolve the promise when parseable XML is returned', function (done) {
+            var req = ts.makeRequest('okay');
+            req.then(function () {
+                Q.isFulfilled(req).should.be.ok;
+                done();
+            });
+        });
+        it('should reject the promise when the request returns an error', function (done) {
+            var req = ts.makeRequest('bad');
+            req.fail(function () {
+                Q.isRejected(req).should.be.ok;
+                done();
+            });
         });
 
-        it('should resolve the promise when parseable XML is returned');
-        it('should reject the promise when unparseable XML is returned');
+        it('should reject the promise when unparseable XML is returned', function (done) {
+            var req = ts.makeRequest('notXML');
+            req.fail(function () {
+                Q.isRejected(req).should.be.ok;
+                done();
+            });
+        });
         it('should reject the promise when an unexpected statusCode is returned');
     });
 
     describe('#clients()', function () {
-        var scope = nock(ts.host)
-                    .post('/api/clients', 'email=' + ts.email + '&password=' + ts.password)
-                    .reply(201, '<?xml version="1.0" encoding="UTF-8"?>\
-                        <clients type="array">\
-                          <client>\
-                            <id type="integer">12341</id>\
-                            <name>Starfleet Command</name>\
-                          </client>\
-                          <client>\
-                            <id type="integer">12342</id>\
-                            <name>The Vulcans</name>\
-                          </client>\
-                          <client>\
-                            <id type="integer">12343</id>\
-                            <name>The Cardassians</name>\
-                          </client>\
-                        </clients>');
+        var clients = ts.clients();
 
         it('should return a promise', function () {
-            var clients = ts.clients();
             Q.isPromise(clients).should.be.ok;
+        });
+        it('should return an array of clients', function (done) {
+            clients.then(function (arr) {
+                arr.clients.client.should.eql([
+                    {
+                        id: '12341',
+                        name: 'Starfleet Command'
+                    },
+                    {
+                        id: '12342',
+                        name: 'The Vulcans'
+                    },
+                    {
+                        id: '12343',
+                        name: 'The Cardassians'
+                    }
+                ]);
+                done();
+            });
         });
     });
 });
